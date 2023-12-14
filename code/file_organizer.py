b39e20cfbbeec5c16f3321baefb5d7f6017879f3
evans <ekioko404@gmail.com>
2023-12-14 08:26:27 -0800

    First code && Tested

--- code/file_organizer.py
+++ code/file_organizer.py
@@ -1,4 +1,17 @@
+# file_organizer.py
+
 import os
+import shutil
 
 def organize_files(directory_path):
-    pass
+    for filename in os.listdir(directory_path):
+        if os.path.isfile(os.path.join(directory_path, filename)):
+            file_extension = filename.split('.')[-1]
+            destination_directory = os.path.join(directory_path, file_extension)
+
+            # Create the destination directory if it doesn't exist
+            if not os.path.exists(destination_directory):
+                os.makedirs(destination_directory)
+
+            # Move the file to the destination directory
+            shutil.move(os.path.join(directory_path, filename), os.path.join(destination_directory, filename))
---
+++ tests/test_files.py
@@ -0,0 +1,11 @@
+# test_file_organizer.py
+import unittest
+from file_organizer import organize_files
+
+class TestFileOrganizer(unittest.TestCase):
+
+    def test_organize_files(self):
+        # Add test cases
+        pass
+
+    # Add more test cases as needed
